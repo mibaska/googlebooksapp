80280b9234055cd4bb0d25ca342ec529385dad86
Michael Baska <mikebaska1@gmail.com>
2020-05-09 17:23:41 -0500

    react-create-app on Heroku

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
 
 app.use(routes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {

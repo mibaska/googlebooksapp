037ac29201c3b6b14164f84e453d107040b374d7
Michael Baska <mikebaska1@gmail.com>
2020-05-09 17:01:02 -0500

    react-create-app on Heroku

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import ServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-ServiceWorker.unregister();
+serviceWorker.unregister();

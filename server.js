9bdfbdf567038e6b81a7d6e9d2974bd72f789dd6
Michael Baska <mikebaska1@gmail.com>
2020-05-09 21:37:51 -0500

    react-create-app on Heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "client/public", "index.html"));
 });
 
 app.listen(PORT, () => {
